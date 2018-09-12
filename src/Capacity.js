8c022ca156a5bac783e228c3ebc2c4b2ffd1bca7
Bibi Collins <bibicollins94@googlemail.com>
2018-09-12 06:25:28 +0100

    TEST AND FEATURES Dynamic capacity and prices

--- src/Capacity.js
+++ src/Capacity.js
@@ -2,17 +2,37 @@ import React, { Component } from 'react';
 import PhoneData from './data/phones.json'
 import css from './Capacity.css'
 export class Capacity extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      index: 0,
+    }
+
+this.sixtyFourCapacity = this.sixtyFourCapacity.bind(this)
+this.twoHundredFiftySix = this.twoHundredFiftySix.bind(this)
+}
+sixtyFourCapacity() {
+  this.setState({index: 0})
+  console.log('sixy four was clicked')
+  }
+twoHundredFiftySix() {
+  this.setState({index: 3})
+  console.log('two hundred and fifty six was clicked')
+}
   render() {
     return (
       <div >
+      <button className="64" onClick={this.sixtyFourCapacity}>64GB</button>
+      <button className="256" onClick={this.twoHundredFiftySix}>256GB</button>
       {PhoneData.map((phone, index)=> {
         return <div className="capacity" key={index}>
-        <h4>Capacity: {phone.deviceSummary[0].memory}</h4>
+        <h4>Capacity: {phone.deviceSummary[this.state.index].memory}</h4>
         <div className="price1">
-        <h4 id="oneOff">From £{phone.deviceSummary[0].priceInfo.hardwarePrice.oneOffPrice.gross} upfront cost</h4>
+        <h4 id="oneOff">From £{phone.deviceSummary[this.state.index].priceInfo.hardwarePrice.oneOffPrice.gross} upfront cost</h4>
         </div>
         <div className="price2">
-        <h4 id="month">When you pay £{phone.deviceSummary[0].priceInfo.bundlePrice.monthlyPrice.gross} a month</h4>
+        <h4 id="month">When you pay £{phone.deviceSummary[this.state.index].priceInfo.bundlePrice.monthlyPrice.gross} a month</h4>
         </div>
         </div>
 
--- src/Capacity.test.js
+++ src/Capacity.test.js
@@ -7,11 +7,15 @@ describe('Capacity', () => {
   it('renders the current capacity', () => {
     expect(capacity.find('div').first().text()).toContain('Capacity: 64GB')
   });
-  it('renders the current upfront price', () => {
-    expect(capacity.find('div').first().text()).toContain('From £')
-    expect(capacity.find('div').first().text()).toContain('upfront cost')
+  
+  it('changes the current monthly price if 256GB button clicked', () => {
+    capacity.find('button').at(1).simulate('click');
+    expect(capacity.find('div').first().text()).toContain('When you pay £46.80 a month')
+    expect(capacity.find('div').first().text()).toContain('From £275 upfront cost')
   })
-  it('renders the current monthly price', () => {
-    expect(capacity.find('div').first().text()).toContain('When you pay £')
+  it('changes the current monthly price if 64GB button clicked', () => {
+    capacity.find('button').at(0).simulate('click');
+    expect(capacity.find('div').first().text()).toContain('When you pay £43.20 a month')
+    expect(capacity.find('div').first().text()).toContain('From £149 upfront cost')
   })
 });
