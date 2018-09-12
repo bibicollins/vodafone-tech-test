d60879b91f9085592e9dcf4808d86800c0effb45
Bibi Collins <bibicollins94@googlemail.com>
2018-09-12 03:54:59 +0100

    TEST Render the description

--- src/Title.test.js
+++ src/Title.test.js
@@ -11,5 +11,8 @@ describe('Title', () => {
   });
   it('renders the rating', () => {
     expect(title.find('h2').text()).toEqual('4.2')
+  });
+  it('renders the description', () => {
+    expect(title.find('h3').text()).toEqual('All-glass design, advanced cameras, wireless charging and a smart A11 Bionic chip. Intelligence never looked better.')
   })
 });
